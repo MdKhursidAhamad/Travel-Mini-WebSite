10a310b6638b04552e9c5194b6a56f5a7aa76ef8
MdKhursidAhamad <mdkhursidahamad49@gmail.com>
2025-08-30 11:38:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+    
+    let navitem = document.getElementById("navigation");
+    let mobileicon = document.getElementById("mobileicon");
+
+    mobileicon.addEventListener("click", function(e){
+        navitem.classList.toggle("active");
+       
+        
+    });
